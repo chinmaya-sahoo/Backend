f926c42e793f02f5b49323b47bb45f8f6e44facf
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-12-15 19:22:52 +0530

    day-2 backend

---
+++ file.js
@@ -0,0 +1,47 @@
+const fs  = require('fs');
+
+//Synchronous write operations
+// fs.writeFileSync('./test.txt','hay there !')
+// fs.writeFileSync('./test.txt','hellow world !')// overwrite
+
+
+//asynchronous write operations
+// fs.writeFile('./test.txt','copy cat!',(error) => {});
+
+// const result = fs.readFileSync('./contact.txt','utf-8');
+
+// console.log(result);
+
+fs.readFile('./contact.txt','utf-8',
+    (error,result) => {
+        if(error) {
+            // console.log(error);
+        }
+        else{
+            // console.log(result);
+        }
+    }
+)
+
+// synchronous operations returns result
+// But asynchronous operations expects a callback
+
+
+
+
+// appending files
+
+// fs.appendFileSync('./contact.txt','\n' + new Date().getTime().toLocaleString());
+// fs.appendFileSync('./contact.txt',`\n Logged in at ${Date.now()}`);
+
+
+
+
+
+
+// coping files
+// fs.cpSync('./contact.txt', './copy.txt');
+// source to destination coping files.
+
+
+
--- hello.js
+++ hello.js
@@ -8,16 +8,16 @@ const math = require('./math') //own creatd math function
 // console.log(math);
 // console.log(math(2,3)); exporting only add fuunction
 
-console.log(math.sub(2,3)); // exporting multiple functions
+// console.log(math.sub(2,3)); // exporting multiple functions
 
 
 
 const { add , sub } = require('./math') ; // importing the object as it is
 
-console.log(add(2,3)); 
+// console.log(add(2,3)); 
 
 
 // to search in built in libraries
 const builtin = require('fs'); // for file system
 
-console.log(builtin);
+// console.log(builtin);
