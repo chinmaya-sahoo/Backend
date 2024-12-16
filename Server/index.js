4b9395172b47ea65689d9935807a63e9fdcebff6
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-12-16 20:14:06 +0530

    Server in node js

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,25 @@
 const http = require('http');
+const fs = require('fs');
 const myServer = http.createServer((req, res)=>{
-    console.log("New request received ");
-    res.end("Hello from server ");
+    // console.log(req.headers);
+    const log = `${Date.now()}: ${req.url} New request received\n`;
+    fs.appendFile("log.txt",log,()=> {
+        // console.log("New request received ");
+        // res.end("Hello from server ");
+        switch(req.url){
+            case '/':
+                res.end("Homepage");
+                break;
+            case '/about':
+                res.end("About");
+                break;
+            default:
+                res.end("404");
+        }
+    });
 });
 
 myServer.listen(8000,()=> console.log("server started!"));
+
+
 
