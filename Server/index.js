3afaffe8cc1419d277f42b9efa0f3f8ed074f588
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-12-16 20:58:10 +0530

    Handling URL

--- Server/index.js
+++ Server/index.js
@@ -1,17 +1,22 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 const myServer = http.createServer((req, res)=>{
+    if(req.url === '/favicon.ico') return res.end();
     // console.log(req.headers);
     const log = `${Date.now()}: ${req.url} New request received\n`;
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
     fs.appendFile("log.txt",log,()=> {
         // console.log("New request received ");
         // res.end("Hello from server ");
-        switch(req.url){
+        switch(myUrl.pathname){
             case '/':
                 res.end("Homepage");
                 break;
             case '/about':
-                res.end("About");
+                const id = myUrl.query.id;
+                res.end("About" + id);
                 break;
             default:
                 res.end("404");
