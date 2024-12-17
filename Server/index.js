33834a75ff4e95a88d57ffde7dea01c10c8177b8
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-12-17 20:31:25 +0530

    day-4 of backend

--- Server/index.js
+++ Server/index.js
@@ -4,9 +4,9 @@ const url = require('url');
 const myServer = http.createServer((req, res)=>{
     if(req.url === '/favicon.ico') return res.end();
     // console.log(req.headers);
-    const log = `${Date.now()}: ${req.url} New request received\n`;
+    const log = `${Date.now()}: ${req.method} ${req.url} New request received\n`;
     const myUrl = url.parse(req.url, true);
-    console.log(myUrl);
+    // console.log(myUrl);
     fs.appendFile("log.txt",log,()=> {
         // console.log("New request received ");
         // res.end("Hello from server ");
@@ -17,6 +17,10 @@ const myServer = http.createServer((req, res)=>{
             case '/about':
                 const id = myUrl.query.id;
                 res.end("About" + id);
+                break;
+            case '/signup':
+                if(req.method === "GET") res.end("This is the signup form");
+                else if(req.method === "POST") res.end("success");
                 break;
             default:
                 res.end("404");
