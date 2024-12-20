2f977463dbe488c616044a70a6d1103c775a4060
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-12-20 14:21:49 +0530

    day-6 of backend

--- Project01/index.js
+++ Project01/index.js
@@ -3,18 +3,59 @@ const app = express();
 const users = require('./MOCK_DATA.json');
 const PORT = 8000;
 
-app.get('/api/users', (req, res) => {
-    res.json(users);
-})
-
 app.get('/users', (req, res) => {
     const data = `
         <ul>
             ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
         </ul>
     `;
-    res.send(data);
+    return res.send(data);
 });
 
+app.get('/api/users', (req, res) => {
+    return res.json(users);
+})
+
+app.get('/api/users/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+})
+
+app.post('/api/users', (req, res) => {
+    return res.json({ status : "pending"});
+})
+
+app.patch('/api/users/:id', (req, res) => {
+    return res.json({ status : "pending"});
+})
+
+app.delete('/api/users/:id', (req, res) => {
+    return res.json({ status : "pending"});
+})
+
+
+
+
+
+/*
+//another way of request handling
+
+app
+.route('/api/users/:id')
+.get((req, res) => {
+    return res.json(users);
+})
+.post((req, res) => {
+    return res.json({ status : "pending"});
+})
+.patch((req, res) => {
+    return res.json({ status : "pending"});
+})
+.delete((req, res) => {
+    return res.json({ status : "pending"});
+})
+
+*/
 
 app.listen(PORT, ()=> console.log(`Server Started at ${PORT}`));
