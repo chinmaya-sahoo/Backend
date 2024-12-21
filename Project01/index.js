9e88c592b0364a3183e2f2ad76f4fe8246b0e637
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-12-21 20:52:32 +0530

    status code done

--- Project01/index.js
+++ Project01/index.js
@@ -36,21 +36,27 @@ app.get('/users', (req, res) => {
 });
 
 app.get('/api/users', (req, res) => {
+    res.setHeader("X-MYNAME" , "chinmaya sahoo");
+    console.log(req.headers)
     return res.json(users);
 })
 
 app.get('/api/users/:id', (req, res) => {
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
+    if(!user) return res.status(404).json({error : " user not found"});
     return res.json(user);
 })
 
 app.post('/api/users', (req, res) => {
     const body = req.body;
+    if(!body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title){
+        return res.status(400).json({ msg : "All Fields Are Required"})
+    }
     // console.log("Body : ",body);
     users.push({ id : users.length + 1 , ...body });
     fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err, data) => {
-        return res.json({ status : "success" , newId : users.length });
+        return res.status(201).json({ status : "success" , newId : users.length });
     });
 })
 
